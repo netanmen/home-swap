97089e841500019bbcf197b59702148000c76717
Netanel Mendel <netanel.cybrtx@outlook.com>
2019-05-20 12:10:36 +0300

    adding front-end register and login

---
+++ HomeSwapApp-SPA/src/app/register/register.component.ts
@@ -0,0 +1,30 @@
+import { Component, OnInit, Input, EventEmitter, Output } from '@angular/core';
+import { AuthService } from '../_services/auth/auth.service';
+
+@Component({
+  selector: 'app-register',
+  templateUrl: './register.component.html',
+  styleUrls: ['./register.component.css']
+})
+export class RegisterComponent implements OnInit {
+  @Output() registerMode = new EventEmitter();
+  model: any = {};
+
+  constructor(private authService: AuthService) { }
+
+  ngOnInit() {
+  }
+
+  register() {
+    this.authService.register(this.model)
+      .subscribe(
+        () => console.log('registered successfully'),
+        error => console.log(error)
+      );
+  }
+
+  cancel() {
+    this.registerMode.emit(false);
+    console.log('cancelled');
+  }
+}
--- HomeSwapApp.API/Controllers/ValuesController.cs
+++ HomeSwapApp.API/Controllers/ValuesController.cs
@@ -21,6 +21,7 @@ namespace HomeSwapApp.API.Controllers
         }
 
         // GET api/values
+        [AllowAnonymous]
         [HttpGet]
         public async Task<IActionResult> GetValuesAsync()
         {
